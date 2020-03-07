88f61fe18cd9e7ef56825c998f0985485a516da8
MSalAguila <msalaguila@gmail.com>
2020-03-07 10:25:44 +0000

    Updating Pods

---
+++ Nippfy/Delegates/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  Nippfy
+//
+//  Created by Manuel Salvador del Águila on 29/02/2020.
+//  Copyright © 2020 Manuel Salvador del Águila. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
